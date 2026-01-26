7c9a35fb78993a2c7dbfb40b3753e40162f3007b
@rizwan.ullah <rizwan.ullah@plcgroup.com>
2026-01-26 12:10:12 +0500

    initail commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center text-2xl">
+      <h4 className="text-2xl text-red-600">hello worldd </h4>
+    </div>
+  );
+};
+
+export default App;
