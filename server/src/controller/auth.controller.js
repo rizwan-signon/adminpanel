a69fee2a9b7150e2bb2e85bcdb43737677902c35
rizwan-signon <rizwan.signon@gmail.com>
2026-01-28 17:26:05 +0500

    add login controller for user that is registred

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,5 +1,36 @@
-const registerUser = (req, res) => {
-  const { fullName, email, password, phone } = req.body;
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+const registerUser = async (req, res) => {
+  try {
+    const { fullName, email, password, phone } = req.body;
+    const hashPassword = await bcryptjs.hash(password, 10);
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashPassword,
+      phone,
+    });
+
+    res.json({ message: "User registered successfully", userId: newUser._id });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    res.json({ message: "Login successful", user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+export { registerUser, loginUser };
